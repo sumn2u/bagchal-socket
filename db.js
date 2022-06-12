6ac1fc85a0ac074834467a50a6352aaf84ec58b4
suman <suman.kunwar@daliaresearch.com>
2022-06-12 17:18:04 -0500

    fix: 🐛 move sql database to root level

--- db.js
+++ db.js
@@ -1,22 +1,23 @@
-var fs = require('fs');
-var dbFile =  "./.data/sqlite.db";
+var fs = require("fs");
+var dbFile = "./sqlite.db";
 var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(dbFile);
-
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function () {
-    if (!exists) {
-      
-        db.run('CREATE TABLE Matches (context TEXT, data TEXT, socketId TEXT, friendSocketId TEXT)', console.log);
-        // insert default matches
-       // db.serialize(function () {
-            //db.run('INSERT INTO Matches (context, data) VALUES ("123", "{\'lives\': 3}")', console.log);
-        //});
-    }
+  if (!exists) {
+    db.run(
+      "CREATE TABLE Matches (context TEXT, data TEXT, socketId TEXT, friendSocketId TEXT)",
+      console.log
+    );
+    // insert default matches
+    // db.serialize(function () {
+    //db.run('INSERT INTO Matches (context, data) VALUES ("123", "{\'lives\': 3}")', console.log);
+    //});
+  }
 });
 
 module.exports = function (app) {
-    app.db = db;
-}
+  app.db = db;
+};
